3940130dad810a227dc33c8c34dcdb585d223e24
alanpaterson82 <alan_paterson82@hotmail.com>
2024-04-12 12:09:58 +0000

    NavBar burger toggle with a custom hook

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://django-rest-ap-9a62d525c1f0.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
